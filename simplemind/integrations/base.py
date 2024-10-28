7eca40712bf97ba4b91c2a253a8f609befe6325e
Kenneth Reitz <me@kennethreitz.org>
2024-10-28 06:26:39 -0400

    test

---
+++ simplemind/integrations/base.py
@@ -0,0 +1,27 @@
+import os
+
+class AIProviderClient:
+
+    def __init__(self, *, api_key=None, environ_name=None):
+        self._environ_name = environ_name
+        # TODO: reverse order, potentially?
+        self._api_key = api_key or self._load_from_environ()
+
+    def _load_from_environ(self):
+        if self._environ_name:
+            self.api_key = os.environ.get(self._environ_name)
+        else:
+            self.api_key = None
+
+    def test_connection(self):
+        raise NotImplementedError("This method must be implemented by the AI provider client.")
+
+    def generate_response(self, request):
+        raise NotImplementedError("This method must be implemented by the AI provider client.")
+
+    def health_check(self):
+        raise NotImplementedError("This method must be implemented by the AI provider client.")
+
+
+    # TODO: logging provider.
+    #
