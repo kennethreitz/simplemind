4f3fcac02df473bebd3d8be748c687963c2f873d
Kenneth Reitz <me@kennethreitz.org>
2024-10-29 14:29:46 -0400

    Refactor generate_data.py to use correct conversation setup and formatting

--- examples/generate_data.py
+++ examples/generate_data.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Iterator
 
 from pydantic import BaseModel
 
@@ -23,18 +23,22 @@ class MovieQuote(BaseModel):
 
 class QuotesList(BaseModel):
     quotes: List[MovieQuote]
-    theme: str
 
 
-quotes = sm.generate_data(
-    llm_provider="openai",
-    llm_model="gpt-4o-mini",
-    prompt="Generate 20 quotes from famous movies",
-    response_model=QuotesList,
-)
+def gen_quotes(n=10) -> Iterator[MovieQuote]:
+    """Generate a list of quotes from famous movies."""
+
+    for q in sm.generate_data(
+        llm_provider="openai",
+        llm_model="gpt-4o-mini",
+        prompt=f"Generate {n} quotes from famous movies",
+        response_model=QuotesList,
+    ).quotes:
+        yield q
+
 
 if __name__ == "__main__":
-    for quote in quotes.quotes:
+    for quote in gen_quotes(n=20):
         print(
             f"{quote.charecter.name} from {quote.movie.title} ({quote.movie.year}): {quote.quote!r}"
         )
