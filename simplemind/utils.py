1ecd4a496659d2883e9ba53086fc4db6cbe11d5c
Francisco Griman <grihardware@gmail.com>
2024-10-30 02:08:12 -0400

    Refine error handling in find_provider function to suggest a single similar provider name

--- simplemind/utils.py
+++ simplemind/utils.py
@@ -13,9 +13,9 @@ def find_provider(provider_name: Union[str, None]):
                 return provider_class()
     
     providers_name = [provider.NAME.lower() for provider in providers]
-    providers_founds = difflib.get_close_matches(provider_name.lower(), providers_name)
+    provider_found = difflib.get_close_matches(provider_name.lower(), providers_name, n=1) # Show only one suggestion
     
-    if providers_founds:
-        raise ValueError(f"Provider {provider_name} not found. Maybe you try to use is '{providers_founds[0]}'?")
+    if provider_found:
+        raise ValueError(f"Provider {provider_name} not found. Maybe you try to use is '{provider_found[0]}'?")
     else:
         raise ValueError(f"Provider {provider_name} not found.")
