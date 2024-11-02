e47ada4598b2e11fcb04bb16aeb4efa4e23a30a6
Kenneth Reitz <me@kennethreitz.org>
2024-11-02 11:08:52 -0400

    Update version to v0.2.2 in conf.py and pyproject.toml

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import simplemind
 project = "simplemind"
 copyright = "2024 Kenneth Reitz"
 author = "Kenneth Reitz"
-release = "v0.2.0"
+release = "v0.2.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
